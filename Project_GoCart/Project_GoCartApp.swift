fb3ad6e2a0f31a12f7b80bfa8f18234b97b66ea7
Gil Dobrovinsky <gdobrovinsky@gmail.com>
2023-02-17 12:10:30 -0500

    Initial Commit

---
+++ Project_GoCart/Project_GoCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_GoCartApp.swift
+//  Project_GoCart
+//
+//  Created by Gil on 2023-02-17.
+//
+
+import SwiftUI
+
+@main
+struct Project_GoCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
